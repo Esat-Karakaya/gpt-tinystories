0e8c91b817cf11f554a7e15436faaab1b71e0e4e
Esat-Karakaya <esat1karakaya@gmail.com>
2025-12-10 15:09:35 +0300

    standartizing MHA and ditching conv1d for nn.Linear

--- model.py
+++ model.py
@@ -94,57 +94,20 @@ class DecoderBlock(nn.Module):
 class MultiheadAttention(nn.Module):
     # Attention sub-block of decoder block
     def __init__(self, config):
-        super(MultiheadAttention, self).__init__()
-        mx_pos = config['max_position_embeddings']
-        self.register_buffer("bias",
-            torch.tril(torch.ones((mx_pos,mx_pos), dtype=torch.bool)).view(1, 1, mx_pos, mx_pos),
-            persistent=False,
+        super().__init__()
+        self.mha = nn.MultiheadAttention(
+            embed_dim=config["hidden_size"],
+            num_heads=config["num_heads"],
+            dropout=config["attn_dropout"],
         )
-        self.embed_dim = config['hidden_size']
-        self.num_heads = config['num_heads']
-        self.head_dim = self.embed_dim // self.num_heads
-        self.split_size = self.embed_dim
-        self.c_attn = Conv1D(3 * self.embed_dim, self.embed_dim)
-        self.c_proj = Conv1D(self.embed_dim, self.embed_dim)
-        self.attn_dropout = nn.Dropout(0.1)
         self.resid_dropout = nn.Dropout(0.1)
-   
-    def _split_heads(self, tensor, num_heads, attn_head_size):
-        new_shape = tensor.size()[:-1] + (num_heads, attn_head_size)
-        tensor = tensor.view(new_shape)
-        return tensor.permute(0, 2, 1, 3)  # B, NH, T, E
-
-    def _merge_heads(self, tensor, num_heads, attn_head_size):
-        tensor = tensor.permute(0, 2, 1, 3).contiguous()
-        new_shape = tensor.size()[:-2] + (num_heads * attn_head_size,)
-        return tensor.view(new_shape) # B, T, H
-
-    def _attn(self, query, key, value, attention_mask=None):
-        attn_weights = torch.matmul(query, key.transpose(-1, -2)) # (B, NH, T, E) x (B, NH, E, T) -> (B, NH, T, T)
-        query_length, key_length = query.size(-2), key.size(-2)
-        # attend only to previous tokens
-        causal_mask = self.bias[:, :, key_length - query_length : key_length, :key_length]
-        # next 3 lines add -inf to the parts where causal mask is 0 (upper diagonal) so that softmax discards future tokens
-        mask_value = torch.finfo(attn_weights.dtype).min # -inf
-        # creates a scalar tensor with value -inf
-        mask_value = torch.full([], mask_value, dtype=attn_weights.dtype, device=attn_weights.device)
-        attn_weights = torch.where(causal_mask, attn_weights.to(attn_weights.dtype), mask_value)
-        attn_weights = nn.functional.softmax(attn_weights, dim=-1)
-        attn_weights = self.attn_dropout(attn_weights)
-        attn_output = torch.matmul(attn_weights, value) # (B, NH, T, T) x (B, NH, T, E) -> (B, NH, T, E)
-        return attn_output, attn_weights
 
     def forward(self, x, attention_mask=None):
         # x: B, T, H
-        query, key, value = self.c_attn(x).split(self.split_size, dim=2) # q, k, h: B, T, H
-        query = self._split_heads(query, self.num_heads, self.head_dim) # B, NH, T, E
-        key = self._split_heads(key, self.num_heads, self.head_dim)
-        value = self._split_heads(value, self.num_heads, self.head_dim)
-        attn_output, attn_weights = self._attn(query, key, value, attention_mask) # B, NH, T, E
-        attn_output = self._merge_heads(attn_output, self.num_heads, self.head_dim) # B, T, H
-        attn_output = self.c_proj(attn_output) # B, T, H
+        attn_output, _ = self.mha(x, x, x, attn_mask=attention_mask)
         attn_output = self.resid_dropout(attn_output)
         return attn_output
+
 
 class MLP(nn.Module):
     # Feedforward NN sub-block of decoder block
@@ -152,8 +115,8 @@ class MLP(nn.Module):
         super().__init__()
         embed_dim = config['hidden_size']
         intermediate_size = 4 * embed_dim
-        self.c_fc = Conv1D(intermediate_size, embed_dim)
-        self.c_proj = Conv1D(embed_dim, intermediate_size)
+        self.c_fc = nn.Linear(embed_dim, intermediate_size)
+        self.c_proj = nn.Linear(intermediate_size, embed_dim)
         self.act = nn.GELU()
         self.dropout = nn.Dropout(0.1)
 
@@ -163,19 +126,3 @@ class MLP(nn.Module):
         hidden_states = self.c_proj(hidden_states)
         hidden_states = self.dropout(hidden_states)
         return hidden_states
-
-class Conv1D(nn.Module):
-    # huggingface gpt2 implementation uses this as an alternative to nn.Linear
-    def __init__(self, out_dim, in_dim):
-        super().__init__()
-        self.out_dim = out_dim
-        self.weight = nn.Parameter(torch.empty(in_dim, out_dim))
-        self.bias = nn.Parameter(torch.zeros(out_dim))
-        nn.init.normal_(self.weight, std=0.02)
-
-    def forward(self, x):
-        size_out = x.size()[:-1] + (self.out_dim,)
-        # matmul then add bias
-        x = torch.addmm(self.bias, x.view(-1, x.size(-1)), self.weight)
-        x = x.view(size_out)
-        return x
