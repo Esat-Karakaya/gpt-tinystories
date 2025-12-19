c109ae63b681837d8b8009f9456f781fc7bb6dcf
Esat-Karakaya <esat1karakaya@gmail.com>
2025-12-19 11:39:46 +0300

    not important

--- play.py
+++ play.py
@@ -1,5 +1,34 @@
-from transformers import AutoTokenizer
+from config8M import cfg
+from tqdm import tqdm
 
-tokenizer = AutoTokenizer.from_pretrained("abakirci/admbkrc-turkish-tokenizer")
+from datasets import load_dataset
+from itertools import islice
 
-print(tokenizer)
+step=0
+tokenizer = cfg.tokenizer
+idx_cnt = 0
+max_step=200
+
+print("loading dataset")
+dataset = load_dataset(
+    "umarigan/tinystories_tr",
+    split="train",
+    streaming=True
+)
+
+ds = list(islice(dataset, max_step))  # only 200 samples
+
+print("loaded!")
+
+for i,story in tqdm(enumerate(ds), total=max_step):
+    str=story["text"]
+    idx = tokenizer.encode(str)
+    if i==0:
+        print(idx)
+    idx_cnt += len(idx)
+    if max_step==step:
+        break
+
+print(idx_cnt)
+
+# 105509
